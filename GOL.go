66215dd7e9979aa6e035c5885d1eac203c91118b
devarshshah15 <devarshshah98@gmail.com>
2020-08-21 23:18:25 -0400

    Golang: Added Comments

--- GOL.go
+++ GOL.go
@@ -1,61 +1,76 @@
 //Referred awk code from gist.github/timm
 //Author:Devarsh Shah 
 package main
+
 import (
-  "fmt"
-  "math/rand"
-  "time"
-  "os"
+   "fmt"
+   "math/rand"
+   "os"
+   "time"
 )
-func life(rows int ,cols int ,some float64 , generations int){
-   var length int=rows*cols
-   var c =make([]int,length)
-   for i:=0;i<length;i++{
-      if(rand.Float64()< some){
-      c[i]=1}else{
-      c[i]=0}
+
+//Function to initialize the matrix with cells
+func life(rows int, cols int, threshold float64, generations int) {
+   var length int = rows * cols
+   var initialState = make([]int, length)
+   for i := 0; i < length; i++ {
+      if rand.Float64() < threshold {
+         initialState[i] = 1
+      } else {
+         initialState[i] = 0
+      }
    }
-   live(c,rows,generations,length)
+   live(initialState, rows, generations, length)
 
 }
-func live(c []int, rows int, generations int, length int){
-  
-  if(generations<1){
-   os.Exit(0)
+
+//Function to generate neighbours based on GOL's 4 rules
+func live(currentState []int, rows int, generations int, length int) {
+
+   if generations < 1 {
+      os.Exit(0)
    }
    var neighbours int
    fmt.Printf("\033[1;1H")
-   fmt.Printf("\033[2J") 
-   fmt.Printf("Generation %d\n",generations)
-   var count int =0
-   for x:=0;x<len(c);x++{
-   if(c[x]==1){
-   fmt.Print("*")}else{
-   fmt.Print(" ")}
-   count+=1
-   if((count%rows)==0){
-   fmt.Println()
+   fmt.Printf("\033[2J")
+   fmt.Printf("Generation %d\n", generations)
+   var count int = 0
+   for x := 0; x < len(currentState); x++ {
+      if currentState[x] == 1 {
+         fmt.Print("*")
+      } else {
+         fmt.Print(" ")
+      }
+      count += 1
+      if (count % rows) == 0 {
+         fmt.Println()
+      }
    }
+   var nextState = make([]int, length)
+   for x := 0; x < len(currentState); x++ {
+      neighbours = currentState[((x-1)%length+length)%length] + currentState[((x+1)%length+length)%length] + currentState[((x-rows-1)%length+length)%length] + currentState[((x-rows)%length+length)%length] + currentState[((x-rows+1)%length+length)%length] + currentState[((x+rows-1)%length+length)%length] + currentState[((x+rows)%length+length)%length] + currentState[((x+rows+1)%length+length)%length]
+      nextState[x] = currentState[x]
+      if currentState[x] == 0 {
+         if neighbours == 3 {
+            nextState[x] = 1
+         } else {
+            nextState[x] = 0
+         }
+      } else {
+         if neighbours == 2 || neighbours == 3 {
+            nextState[x] = 1
+         } else {
+            nextState[x] = 0
+         }
+      }
+
    }
-   var b =make([]int,length)
-   for x:=0;x<len(c);x++{
-   neighbours=c[((x-1)%length+length)%length]+c[((x+1)%length+length)%length]+c[((x-rows-1)%length+length)%length]+c[((x-rows)%length+length)%length]+c[((x-rows+1)%length+length)%length]+c[((x+rows-1)%length+length)%length]+c[((x+rows)%length+length)%length]+c[((x+rows+1)%length+length)%length]
-    b[x]=c[x]
-    if(c[x]==0){
-    if(neighbours==3){
-    b[x]=1
-    }else{
-    b[x]=0
-    }}else{
-    if(neighbours==2 || neighbours==3){
-    b[x]=1}else{
-    b[x]=0}}
-    
-    }
-    generations--
-    time.Sleep(time.Second/2)
-    live(b,rows,generations,length)
-  }
+   generations--
+   time.Sleep(time.Second / 2)
+   live(nextState, rows, generations, length)
+}
+
+//Main function 
 func main() {
-life(50,20,0.3,20)
+   life(50, 20, 0.3, 20)
 }
