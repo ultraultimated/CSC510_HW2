b06ba835dbe12b978319e1be3e0276d9a09e7b4d
devarshshah15 <devarshshah98@gmail.com>
2020-08-26 14:50:43 -0400

    Updated GOL.go

--- code/GO/GOL.go
+++ code/GO/GOL.go
@@ -67,14 +67,7 @@ func live(currentState [30]int, rows int, generations int, length int) {
    for x := 0; x < len(currentState); x++ {
       //-7%2=-1 in golang, so ((x-1)%length+length)%length gives -7%2=1
       
-      neighbours = currentState[((x-1)%length+length)%length] + currentState[((x+1)%length+length)%length] + currentState[((x-rows-1)%length+length)%length] + currentState[((x-rows)%length+length)%length] + currentState[((x-rows+1)%length+length)%length] + currentState[((x+rows-1)%length+length)%length] + currentState[((x+rows)%length+length)%length] + currentState[((x+rows+1)%length+length)%length]
-      nextState[x] = currentState[x]
-      if currentState[x] == 0 {
-         if neighbours == 3 {
-            nextState[x] = 1
-         } else 
-         {
-            nextState[x] = 0
+     
          }
       } else 
       {
